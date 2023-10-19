a04a510817106c385ce88a9d8fe60e04759b0d55
Ethan-Latymer <115607887+Ethan-Latymer@users.noreply.github.com>
2023-10-19 01:08:22 +0100

    Getting it working with github pages (hopefully)

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
 const IDform = document.getElementById("ID-form");
 const iframeCont = document.getElementById("iframe-container");
 const submitBtn = document.getElementById("submit-label");
+var player = null;
 
-IDform.addEventListener("submit", (event) => {
-    const iframe = `<iframe src="http://www.youtube.com/embed/${IDform["ID"].value}" frameborder="0" allowfullscreen></iframe>`
-    let iframeDiv = document.createElement("div");
+function onYouTubeIframeAPIReady() {
+    IDform.addEventListener("submit", (event) => {        
+        let ID = IDform["ID"].value;
+        if (ID.includes("=")) {
+            ID = ID.slice(ID.indexOf("=") + 1, ID.indexOf("=") + 12)
+        }
+    
+        if (player == null) {
+            player = new YT.Player('player', {
+                frameborder: 0,
+                
+                playerVars: {
+                    'enablejsapi': 1,
+                    'showinfo': 0,
+                    'rel': 0,
+                    'controls': 1
+                },
 
-    submitBtn.classList.add("selected");
-    setTimeout(() => {
-        submitBtn.classList.remove("selected");
-    }, 500);
-
-    if (iframeCont.classList.contains("used")) {
-        iframeCont.innerHTML = '';
-        iframeCont.classList.remove("used");
-    }
-
-    iframeCont.classList.add("used");
-    iframeDiv.innerHTML = iframe;
-    iframeCont.append(iframeDiv);
-    event.preventDefault();
-});
+                events: {
+                    'onReady': onPlayerReady
+                }
+            });
+            
+            function onPlayerReady(event) {
+                event.target.cueVideoById(ID);
+            }
+        }
+        else {
+            player.cueVideoById(ID);
+        }
+    
+        submitBtn.classList.add("selected");
+        setTimeout(() => {
+            submitBtn.classList.remove("selected");
+        }, 500);
+    
+        event.preventDefault();
+    });
+}
